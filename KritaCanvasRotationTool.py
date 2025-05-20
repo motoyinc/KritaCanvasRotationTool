5e437abf494a98d3f12d64bda2dc815bc0a87e86
motoyinc <motoyinc@gmail.com>
2025-05-21 02:38:07 +0800

    [Feature] 新增旋转步进大小设置功能

--- KritaCanvasRotationTool.py
+++ KritaCanvasRotationTool.py
@@ -1,33 +1,50 @@
 from krita import Krita, Extension
+from PyQt5.QtWidgets import QInputDialog
+from PyQt5.QtWidgets import QMenu
 
 class RotateCanvasTool(Extension):
     def __init__(self, parent):
         super().__init__(parent)
+        self.step = 1.0
 
     def setup(self):
         pass
 
     def createActions(self, window):
-        action_cw = window.createAction(
-            "canvasRotateCW",
-            "Rotate Canvas Clockwise",
-            "tools/scripts"
-        )
+        # 创建新的菜单
+        action = window.createAction("canvasRotationMenu", "Krita Canva Rotation Tool", "tools/scripts")
+        menu = QMenu(window.qwindow())
+        action.setMenu(menu)
+
+        # 设置旋转步进
+        action_set = window.createAction("rotateCanvasSetStep", "Set Rotation Step", "")
+        action_set.triggered.connect(self.set_rotation_step)
+        menu.addAction(action_set)
+
+        # 顺时针旋转
+        action_cw = window.createAction("canvasRotateCW", "Rotate Canvas Clockwise" ,"")
         action_cw.triggered.connect(self.rotate_canvas_clockwise)
-        action_ccw = window.createAction(
-            "canvasRotateCCW",
-            "Rotate Canvas CounterClockwise",
-            "tools/scripts"
-        )
+        menu.addAction(action_cw)
+
+        # 逆时针旋转
+        action_ccw = window.createAction("canvasRotateCCW", "Rotate Canvas CounterClockwise", "")
         action_ccw.triggered.connect(self.rotate_canvas_counterclockwise)
+        menu.addAction(action_ccw)
+
+
+    def set_rotation_step(self):
+        """设置旋转步进"""
+        step, ok = QInputDialog.getDouble(None, "Set Rotation Step", "Enter rotation step (degrees):", self.step, 0.1, 45.0, 0.25)
+        if ok:
+            self.step = step
 
     def rotate_canvas_clockwise(self):
         """将当前画布顺时针旋转1度"""
-        self.rotate_canvas(1)
+        self.rotate_canvas(self.step)
 
     def rotate_canvas_counterclockwise(self):
         """将当前画布逆时针旋转1度"""
-        self.rotate_canvas(-1)
+        self.rotate_canvas(-self.step)
 
     def rotate_canvas(self, angle_delta):
         app = Krita.instance()
