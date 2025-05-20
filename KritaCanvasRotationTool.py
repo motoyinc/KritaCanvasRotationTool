94264b7d502c8de8b8b259f8d30e0d64a19a4813
motoyinc <motoyinc@gmail.com>
2025-05-21 03:41:29 +0800

    [feature] 新增序列化到磁盘类

--- KritaCanvasRotationTool.py
+++ KritaCanvasRotationTool.py
@@ -1,13 +1,13 @@
+from .SerializationToolConfig import SerializationToolConfig
 from krita import Krita, Extension
 from PyQt5.QtWidgets import QInputDialog
 from PyQt5.QtWidgets import QMenu
-import configparser
-import os
 
 class RotateCanvasTool(Extension):
     def __init__(self, parent):
         super().__init__(parent)
-        self.step = 1.0
+        self.config = SerializationToolConfig()
+        self.config.read_config_value()
 
     def setup(self):
         pass
@@ -33,44 +33,11 @@ class RotateCanvasTool(Extension):
         action_ccw.triggered.connect(self.rotate_canvas_counterclockwise)
         menu.addAction(action_ccw)
 
-    def update_config_value(self):
-        config_path = os.path.join(os.path.dirname(__file__), 'KritaCanvasRotationToolSettings.ini')
-        config = configparser.ConfigParser()
-
-        if not os.path.exists(config_path):
-            config['CanvasRotation'] = {'step': str(self.step)}
-        else:
-            config.read(config_path)
-            if 'CanvasRotation' not in config:
-                config['CanvasRotation'] = {}
-
-        config['CanvasRotation']['step'] = str(self.step)
-        with open(config_path, 'w') as configfile:
-            config.write(configfile)
-
-    def read_config_value(self):
-        config_path = os.path.join(os.path.dirname(__file__), 'KritaCanvasRotationToolSettings.ini')
-        config = configparser.ConfigParser()
-        config.read(config_path)
-
-        if 'CanvasRotation' in config and 'step' in config['CanvasRotation']:
-            try:
-                # 将读取的字符串转换为浮点数
-                step_value = float(config['CanvasRotation']['step'])
-                return step_value
-            except ValueError:
-                # 如果转换失败，返回默认值
-                return 1.0
-        else:
-            return 1.0
-
     def set_rotation_step(self):
         """设置旋转步进"""
-        self.step = self.read_config_value()
-        step, ok = QInputDialog.getDouble(None, "Set Rotation Step", "Enter rotation step (degrees):", self.step, 0.1, 45.0, 2)
+        step, ok = QInputDialog.getDouble(None, "Set Rotation Step", "Enter rotation step (degrees):", self.config.step, 0.1, 45.0, 2)
         if ok:
-            self.step = step
-            self.update_config_value()
+            self.config.save_config_value(step)
 
     def rotate_canvas_clockwise(self):
         """将当前画布顺时针旋转1度"""
@@ -88,6 +55,6 @@ class RotateCanvasTool(Extension):
             if view is not None:
                 canvas = view.canvas()
                 current_angle = canvas.rotation()
-                canvas.setRotation(current_angle + angle_delta)  # 设置新角度为当前角度减1度
+                canvas.setRotation(current_angle + angle_delta)
 
 Krita.instance().addExtension(RotateCanvasTool(Krita.instance()))
---
+++ SerializationToolConfig.py
@@ -0,0 +1,35 @@
+import configparser
+import os
+
+class SerializationToolConfig:
+    def __init__(self):
+        self.__config_path = os.path.join(os.path.dirname(__file__), 'KritaCanvasRotationToolSettings.ini')
+        self.__config = configparser.ConfigParser()
+
+        ## 配置bi
+        self.step = 1.0
+
+    def read_config_value(self):
+        self.__config.read(self.__config_path)
+
+        if 'CanvasRotation' in self.__config and 'step' in self.__config['CanvasRotation']:
+            try:
+                step_value = float(self.__config['CanvasRotation']['step'])
+                self.step = step_value
+            except ValueError:
+                self.step = 1.0
+        else:
+            self.step = 1.0
+
+    def save_config_to_disk(self, step):
+        self.step = step
+        if not os.path.exists(self.__config_path):
+            self.__config['CanvasRotation'] = {'step': str(self.step)}
+        else:
+            self.__config.read(self.__config_path)
+            if 'CanvasRotation' not in self.__config:
+                self.__config['CanvasRotation'] = {}
+    
+        self.__config['CanvasRotation']['step'] = str(self.step)
+        with open(self.__config_path, 'w') as configfile:
+            self.__config.write(configfile)
